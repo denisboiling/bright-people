a9255fa1f685ae2fd6c1df1ffa38592e9adf7e3e
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-15 15:26:04 +0300

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrightPeople::Application.config.secret_token = 'd57ea0a20752525725ff80496cf228e617376d9f7d1429772f917639032107abd9c7eb150d75e3b468d4e215c64f29d27ec3194cd62ec394723555de28ae10e8'
