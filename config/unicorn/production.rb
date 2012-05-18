fe4613d95d0b64dae8c3a947485ec0f5bc3e9311
Maxim Kremenev <ezotrank@gmail.com>
2012-05-18 16:59:48 +0300

    fix unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -9,9 +9,9 @@ listen "/var/www/bright-people/shared/pids/unicorn.sock", :backlog => 64
 stderr_path "/var/www/bright-people/current/log/unicorn.log"
 stdout_path "/var/www/bright-people/current/log/unicorn.log"
 
-if GC.respond_to?(:copy_on_write_friendly=)
-  GC.copy_on_write_friendly = true
-end
+# if GC.respond_to?(:copy_on_write_friendly=)
+#   GC.copy_on_write_friendly = true
+# end
 
 before_fork do |server, worker|
   old_pid = '/var/www/bright-people/shared/pids/unicorn.pid.oldbin'
@@ -24,25 +24,25 @@ before_fork do |server, worker|
   end
 end
 
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
+# after_fork do |server, worker|
+#   ActiveRecord::Base.establish_connection
 
-  begin
-    uid, gid = Process.euid, Process.egid
-    user, group = 'www-data', 'www-data'
-    target_uid = Etc.getpwnam(user).uid
-    target_gid = Etc.getgrnam(group).gid
-    worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid || gid != target_gid
-      Process.initgroups(user, target_gid)
-      Process::GID.change_privilege(target_gid)
-      Process::UID.change_privilege(target_uid)
-    end
-  rescue => e
-    if RAILS_ENV == 'development'
-      STDERR.puts "couldn't change user, oh well"
-    else
-      raise e
-    end
-  end
-end
+#   begin
+#     uid, gid = Process.euid, Process.egid
+#     user, group = 'www-data', 'www-data'
+#     target_uid = Etc.getpwnam(user).uid
+#     target_gid = Etc.getgrnam(group).gid
+#     worker.tmp.chown(target_uid, target_gid)
+#     if uid != target_uid || gid != target_gid
+#       Process.initgroups(user, target_gid)
+#       Process::GID.change_privilege(target_gid)
+#       Process::UID.change_privilege(target_uid)
+#     end
+#   rescue => e
+#     if RAILS_ENV == 'development'
+#       STDERR.puts "couldn't change user, oh well"
+#     else
+#       raise e
+#     end
+#   end
+# end
