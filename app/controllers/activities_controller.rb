e0ac13e82a8955408509e3043f9297a52ec8b13f
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-18 09:54:59 +0300

    remove unused organizations controller, remove unused code

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,15 +2,4 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
   end
-
-  def new
-    @activity = Activity.new
-  end
-
-  def create
-    @activity = Activity.new(params[:activity])
-    if @activity.save
-    else
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ BrightPeople::Application.routes.draw do
   put 'users/email' => 'users#update_email', as: :update_user_email
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  resources :organizations
-  resources :activities
+  resources :activities, :only => [:index]
 
   root :to => 'home#show'
 end
