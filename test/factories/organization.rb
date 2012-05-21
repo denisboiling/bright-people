1f8d6c5ac233bfb142961c8ba3b9f45ebe7b7c96
Maxim Kremenev <ezotrank@gmail.com>
2012-05-21 10:19:51 +0300

    Remove /test Returned db/scheme.rb to repo. All tests should be pass.

--- test/factories/organization.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :organization do
-    title { Faker::Lorem.words(1).join(" ").to_s() }
-    metro_station { Faker::Lorem.words(1).join(" ").to_s() }
-    address { Faker::Lorem.words(3).join(" ").to_s() }
-  end
-end
