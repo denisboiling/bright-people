59cf8227321e3944faacca2c3861d805d5ac958a
Maxim Kremenev <ezotrank@gmail.com>
2012-05-22 15:01:04 +0300

    fix BRI-32

--- db/schema.rb
+++ db/schema.rb
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(:version => 20120522113822) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "role_id",                                :null => false
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
