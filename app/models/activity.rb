79febb1fe4f018e284198fbce07f1c606ca70edf
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-24 15:18:27 +0300

    bind age tags and activities as many to many

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -35,10 +35,12 @@ class Activity < ActiveRecord::Base
 
   belongs_to :organization
   belongs_to :metro_station
-  belongs_to :age_tag
 
   has_many :activity_direction_relations
   has_many :direction_tags, through: :activity_direction_relations
+
+  has_many :activity_age_relations
+  has_many :age_tags, through: :activity_age_relations
 
   has_many :teachers
   has_many :votes, class_name: 'ActivityVote'
--- db/migrate/20120517130728_bind_activity_and_age_tag_as_one_to_many.rb
+++
@@ -1,5 +0,0 @@
-class BindActivityAndAgeTagAsOneToMany < ActiveRecord::Migration
-  def change
-    add_column :activities, :age_tag_id, :integer
-  end
-end
