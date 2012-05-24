c2e0c9f6f4d18ee88e98db6181387026fc8ef150
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-24 14:40:40 +0300

    bind activity and tags in sample data

--- db/sample/activities.rb
+++ db/sample/activities.rb
@@ -1,4 +1,17 @@
-Activity.all.each do |org|
-  org.update_attribute(:location,
-             Point.from_x_y(54.710000 + "0.00#{rand(9999)}".to_f, 20.500000  + "0.00#{rand(9999)}".to_f, 4326))
+Activity.all.each do |ac|
+  ac.update_attribute :location, Point.from_x_y(54.710000 + "0.00#{rand(9999)}".to_f,
+                                                20.500000  + "0.00#{rand(9999)}".to_f, 4326)
+  rand(1..3).times do
+    tag =
+      if ac.is_educational
+        DirectionTag.educational.sample
+      else
+        DirectionTag.entertainment.sample
+      end
+    ac.direction_tags << tag
+  end
+  
+  tag = AgeTag.all.sample
+  ac.age_tag = tag
+  ac.save!
 end
