2ef5cedd63d8ffb9d535033dee73b3bcc9db10db
khodzha <khodzha.sh@gmail.com>
2012-05-25 17:48:31 +0300

    comments tests rewritten

--- features/support/help_methods.rb
+++ features/support/help_methods.rb
@@ -6,6 +6,22 @@ module HelpSteps
   def specialist_by_name(name)
     User.specialists.find_by_name(name)
   end
+
+  def article_by_title(title)
+    Article.find_by_title(title)
+  end
+
+  def interview_by_title(title)
+    Interview.find_by_title(title)
+  end
+
+  def special_project_by_title(title)
+    SpecialProject.find_by_title(title)
+  end
+
+  def news_by_title(title)
+    News.find_by_title(title)
+  end
 end
 
 World(HelpSteps)
