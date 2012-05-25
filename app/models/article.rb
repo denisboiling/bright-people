9f9c2762c83a9c3210c58fa83b3ec64c53f1b09b
khodzha <khodzha.sh@gmail.com>
2012-05-25 15:37:46 +0300

    Created comments tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,6 +12,4 @@ class Article < ActiveRecord::Base
                     styles: { medium: "300x300>", thumb: "100x100>" },
                     path: ":rails_root/public/system/:attachment/:id/:style/:filename",
                     url: "/system/:attachment/:id/:style/:filename"
-
-  validates_attachment_presence :picture
 end
