c9d31fc411b435525c03fd5bace2e1dd4fad0d1a
khodzha <khodzha.sh@gmail.com>
2012-05-28 16:36:18 +0300

    routes changed to pass cucumber tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 BrightPeople::Application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: [:registrations]
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   put 'users/email' => 'users#update_email', as: :update_user_email
 
   devise_for :admin_users, ActiveAdmin::Devise.config
