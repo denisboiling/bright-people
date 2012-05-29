4111922d4bf625f47ef78364e474f4b7cd99e39c
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-29 10:17:42 +0300

    fix sample data, add active? method for contests

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -2,4 +2,9 @@ class Contest < ActiveRecord::Base
   belongs_to :category, class_name: 'ContestCategory'
   
   attr_accessible :name, :started_at, :ended_at
+  
+  def active?
+    now = DateTime.now
+    (started_at < now) and (now < ended_at)
+  end
 end
