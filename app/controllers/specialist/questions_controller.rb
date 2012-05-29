b5b00572fc510e75c5514c53a36916ae940e1dd0
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-05-29 14:37:49 +0300

    fix pagination calls

--- app/controllers/specialist/questions_controller.rb
+++ app/controllers/specialist/questions_controller.rb
@@ -3,7 +3,7 @@ class Specialist::QuestionsController < Specialist::BaseController
   before_filter :load_object, except: :index
 
   def index
-    @questions = current_user.questions.paginate(page: params[:page], per_page: 10)
+    @questions = current_user.questions.page(params[:page]).per(10)
   end
 
   def show
