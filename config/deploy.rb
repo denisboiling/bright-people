ae5588c433a7f975274b88b6a209db586b79f5f6
Maxim Kremenev <ezotrank@gmail.com>
2012-05-31 10:19:22 +0300

    add whenever to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,13 @@
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
 require 'thinking_sphinx/deploy/capistrano'
+require 'whenever/capistrano'
 
 set :stages, %w(srv)
 set :default_stage, "srv"
 set :repository,  "git@github.com:balticit/bright-people.git"
 set :scm, :git
-set :branch, "master"
+set :branch, "ezo"
 
 set :application, "bright-people"
 
