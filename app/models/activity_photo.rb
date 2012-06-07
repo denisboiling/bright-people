904f9968f5c8a933c6e49ef683c7add263c0fe00
Maxim Kremenev <ezotrank@gmail.com>
2012-06-07 14:57:09 +0300

    fix activity photos and remove from srv-dev deploy assets precompile

--- app/models/activity_photo.rb
+++ app/models/activity_photo.rb
@@ -2,7 +2,7 @@ class ActivityPhoto < ActiveRecord::Base
   belongs_to :activity
 
   has_attached_file :attach,
-  styles: { medium: "300x300>", thumb: "100x100>", slider: '530x370' },
+  styles: { thumb: "100x100>", slider: '530x370#' },
   path: ":rails_root/public/system/activity_photos/:attachment/:id/:style/:filename",
   url: "/system/activity_photos/:attachment/:id/:style/:filename",
   default_style: :thumb
