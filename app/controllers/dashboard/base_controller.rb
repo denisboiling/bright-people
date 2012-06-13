f3d46ed33c0e09f17351b08264df9ac9a977ccc2
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-13 11:13:07 +0300

    simplify authorization rules for dashboard

--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,16 +1,3 @@
 class Dashboard::BaseController < ApplicationController
-  protect_from_forgery
-  # OPTIMIZE: brrr... ugly
-  before_filter :verify_user
-
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: exception.message
-  end
-
-  private
-
-  def verify_user
-    redirect_to root_path unless can? :access, :dashboard
-  end
-
+  load_and_authorize_resource :dashboard, class: false
 end
