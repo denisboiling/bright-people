56ecd4f15da13b90eabd1ccaa8866f2e26723b26
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-15 11:05:34 +0300

    add favourite link with styling

--- app/helpers/favourites_helper.rb
+++ app/helpers/favourites_helper.rb
@@ -1,2 +1,16 @@
+# encoding: utf-8
+
 module FavouritesHelper
+  def favourite_block(object)
+    return '' unless current_user
+    
+    args = { relation_id: object.id,
+             relation_type: object.class.name,
+             user_id: current_user.id }
+    if Favourite.where(args).any?
+      content_tag :span, link_to("Избранное", '#'), class: :favourite_block_is
+    else
+      content_tag :span, link_to("В избранное", '#'), class: :favourite_block_add
+    end
+  end
 end
