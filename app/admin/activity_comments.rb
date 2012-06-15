3c0ac7cedfd204d34a571a900391afc14253771e
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-15 09:39:25 +0300

    fix is_parent attr name

--- app/admin/activity_comments.rb
+++ app/admin/activity_comments.rb
@@ -23,16 +23,16 @@ ActiveAdmin.register ActivityComment do
       f.input :commentator
       f.input :content, input_html: {size: 10}
       f.input :picture, as: :file,
-      hint: f.template.image_tag(f.object.picture.url(:medium))
+                        hint: f.template.image_tag(f.object.picture.url(:medium))
       f.input :activity, as: :select, collection: Activity.all
-      f.input :isParent, as: :boolean
+      f.input :is_parent, as: :boolean
     end
     f.buttons
   end
 
   show do
-    attributes_table :commentator, :content, :isParent,:created_at,
-    :updated_at, :activity
+    attributes_table :commentator, :content, :is_parent,:created_at,
+                     :updated_at, :activity
 
     panel 'Аватар' do
       image_tag(activity_comment.picture.url)
