dab865859c500d4ec93e2e887b7bef327bec0ca4
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-15 17:33:21 +0300

    display special project list with sorting

--- app/controllers/special_projects_controller.rb
+++ app/controllers/special_projects_controller.rb
@@ -6,7 +6,18 @@ class SpecialProjectsController < ApplicationController
   end
 
   def index
-    @special_projects = SpecialProject.all
+    @special_projects = SpecialProject.scoped
+    @special_projects =
+      case params[:sort]
+      when 'created'
+        @special_projects.order('created_at DESC')
+      when 'comments_count'
+        # TODO
+        @special_projects
+      else
+        @special_projects
+      end
+    @special_projects = @special_projects.page(params[:page]).per(15)
   end
 
   def show
