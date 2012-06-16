bc7622ae47ac7cc6afed9d574071bac0a5bebdc5
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-16 13:08:31 +0300

    correct scaling options

--- app/models/special_project.rb
+++ app/models/special_project.rb
@@ -8,7 +8,7 @@ class SpecialProject < ActiveRecord::Base
   has_many :comments, as: :relation
 
   has_attached_file :picture,
-                    styles: { medium: "300x300>", thumb: "160x100>" },
+                    styles: { medium: "300x300^#", thumb: "160x100^#" },
                     path: ":rails_root/public/system/special_projects/:attachment/:id/:style/:filename",
                     url: "/system/special_projects/:attachment/:id/:style/:filename",
                     default_style: :thumb
