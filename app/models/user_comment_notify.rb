6f3193d9f64dd85ac2f8e945fb1ee453025e055c
Gordeev Vladimir <gordeev.vladimir.v@gmail.com>
2012-06-16 12:19:01 +0300

    fix comment notifications

--- app/models/user_comment_notify.rb
+++ app/models/user_comment_notify.rb
@@ -1,14 +1,15 @@
 class UserCommentNotify < ActiveRecord::Base
   belongs_to :comment
+  belongs_to :user
   belongs_to :child_comment, class_name: 'Comment', foreign_key: :child_comment_id
-
-  attr_accessible :child_comment, :comment
+  
+  attr_accessible :child_comment, :comment, :user
 
   class << self
     # Create notify by comment
     def create_notify(_comment)
       return if _comment.top_level?
-      self.create(child_comment: _comment, comment: _comment.parent)
+      self.create(child_comment: _comment, comment: _comment.parent, user: _comment.parent.user)
     end
   end
 end
