e7831d46a140ed8504aae50da8f7f0520e9b59bc
Maxim Kremenev <ezotrank@gmail.com>
2012-06-18 11:55:18 +0300

    fix admin activities

--- app/models/activity_direction_relation.rb
+++ app/models/activity_direction_relation.rb
@@ -2,12 +2,11 @@ class ActivityDirectionRelation < ActiveRecord::Base
   belongs_to :activity
   belongs_to :direction_tag
 
-  validate :kind_match
-
-  private
-
-  def kind_match
-    match = !!activity.is_educational == !!direction_tag.is_educational
-    errors.add(:activity, ", direction kind must match") unless match
-  end
+  # TODO: return back
+  # validate :kind_match
+  # private
+  # def kind_match
+  #   match = !!activity.is_educational == !!direction_tag.is_educational
+  #   errors.add(:activity, ", direction kind must match") unless match
+  # end
 end
