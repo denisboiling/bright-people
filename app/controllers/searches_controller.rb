f865e669bfcf5d4b1738cdf85b3782ba011a52c4
Maxim Kremenev <ezotrank@gmail.com>
2012-06-21 09:19:51 +0300

    fix sponsors to 2

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 class SearchesController < ApplicationController
   def show
-    
     range = case params[:category]
-      when "Москва для детей"
-        [Activity]
-      when "Журнал"
-        [Article]
-      when "Конкурсы"
-        [Contest]
-      when "Экспертный совет"
-        [Sponsor]
-      when "Спецпроекты"
-        [SpecialProject]
-      else
-        [Activity, Article, Sponsor, SpecialProject, Contest]
-    end
+            when "Москва для детей"
+              [Activity]
+            when "Журнал"
+              [Article]
+            when "Конкурсы"
+              [Contest]
+            when "Экспертный совет"
+              [Sponsor]
+            when "Спецпроекты"
+              [SpecialProject]
+            else
+              [Activity, Article, Sponsor, SpecialProject, Contest]
+            end
     @results = ThinkingSphinx.search(params[:q], star: true, classes: range)
     @results = @results.page(params[:page]) if params[:page]
     render partial: 'searches/result', locals: { results: @results } if params[:remote]
