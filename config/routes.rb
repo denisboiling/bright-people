7b6442b21d5cbb1ee51d9087d43d9c6f75c0a982
khodzha <khodzha.sh@gmail.com>
2012-07-09 11:19:18 +0300

    authors page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BrightPeople::Application.routes.draw do
+  match '/authors' => 'users#authors'
   match '/vk' => 'vk_pages#auth'
   match '/vk_save' => 'vk_pages#create_vk_page'
   match '/fb' => 'fb_pages#fb_auth'
