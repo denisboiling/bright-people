a7346f4a41200c54ba30d73a43b894846b035812
Maxim Kremenev <ezotrank@gmail.com>
2012-07-17 13:26:57 +0300

    fix #BRI-190

--- app/models/news_photo.rb
+++ app/models/news_photo.rb
@@ -1,7 +1,7 @@
 class NewsPhoto < ActiveRecord::Base
   belongs_to :news
 
-  has_attached_file :attach, styles: { thumb: "100x100^#", slider: '530x370^#' },
+  has_attached_file :attach, styles: { thumb: "100x100^#", slider: '530x370^#', large: '1366x768^#' },
                              path: ":rails_root/public/system/news_photos/:attachment/:id/:style/:filename",
                              url: "/system/news_photos/:attachment/:id/:style/:filename",
                              default_style: :thumb
