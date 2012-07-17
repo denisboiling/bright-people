9e15ca23e4e0d62a90dd8bb8e94cb92f45587654
Maxim Kremenev <ezotrank@gmail.com>
2012-07-17 15:22:34 +0300

    fix facebook post news and articles

---
+++ app/models/social_post_observer.rb
@@ -0,0 +1,29 @@
+class SocialPostObserver < ActiveRecord::Observer
+  # TODO: rewrite this shit!!!!
+  observe :article, :news
+
+  include ActionView::Helpers::UrlHelper
+
+  def after_create(model)
+    publish(model) if model.published?
+  end
+
+  # TODO: News or Artilce has't field published, replace this shit
+  def after_update(model)
+    if model.published? and !model.changes[:published].nil?
+      publish(model)
+    end
+  end
+
+  def publish(model)
+    unless FbPage.first.nil?
+      page = FbGraph::Page.new(FbPage.first.identifier, :access_token => FbPage.first.token)
+      pic = model.photo ? "http://images.bright-people.ru" + model.picture.url(:medium, false) : nil
+      page.feed!(:message => model.title,
+                 :link => "http://bright-people.ru/#{model.name.downcase.pluralize}/" + model.id.to_s,
+                 :picture => pic
+                )
+    end
+  end
+
+end
