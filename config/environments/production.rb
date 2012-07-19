0e0480afb70f494f0cef3fb1e8fc774e40b53a8d
Maxim Kremenev <ezotrank@gmail.com>
2012-07-19 17:56:51 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ BrightPeople::Application.configure do
   config.assets.digest = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.action_controller.asset_host = "http://images.bright-people.ru"
-  config.assets.precompile += %w( admin/active_admin.js admin/active_admin.css chosen.jquery.min )
+  config.assets.precompile += %w( admin/active_admin.js admin/active_admin.css chosen.jquery.min.js )
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
 end
