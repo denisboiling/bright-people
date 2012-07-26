0c183a3484b47e5b58a1dbfbc62962359703f5a7
Maxim Kremenev <ezotrank@gmail.com>
2012-07-26 18:07:23 +0300

    fix migration

--- db/migrate/20120726123942_add_priority_to_creative_group.rb
+++ db/migrate/20120726123942_add_priority_to_creative_group.rb
@@ -1,5 +1,5 @@
 class AddPriorityToCreativeGroup < ActiveRecord::Migration
   def change
-    add_column :creative_groups, :position, null: false, default: 0
+    add_column :creative_groups, :position, :integer, null: false, default: 0
   end
 end
