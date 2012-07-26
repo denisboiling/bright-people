cd3748640a84a67d80b01ec8d6d32344d0e78bf4
khodzha <khodzha.sh@gmail.com>
2012-07-26 17:56:31 +0300

    added host_name variable to config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -13,4 +13,5 @@ BrightPeople::Application.configure do
                                   dashboard/*.js)
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+  config.host_name = "http://stagebp.balticit.ru/"
 end
