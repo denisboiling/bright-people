ffcc6a541ec14ff1dd1be636b424f60597e5a317
khodzha <khodzha.sh@gmail.com>
2012-07-27 11:50:29 +0300

    hostname fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ BrightPeople::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.assets.compress = false
   config.assets.debug = true
-  config.host_name = "http://bp.balticit.ru"
+  config.host_name = "http://localhost:3000"
   config.vk_public = '-40194424'
   config.fb_page_name = 'Mytestpage'
   config.fb_app_id = '245615425549849'
