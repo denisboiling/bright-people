5d2c7847d3e7abb08480bdb315f9a997156ec0ce
khodzha <khodzha.sh@gmail.com>
2012-07-27 10:45:21 +0300

    improvements in social posting tracking

--- app/models/social_post_observer.rb
+++ app/models/social_post_observer.rb
@@ -11,7 +11,7 @@ class SocialPostObserver < ActiveRecord::Observer
 
   # TODO: News or Artilce has't field published, replace this shit
   def after_update(model)
-    if model.published? and !model.changes[:published].nil?
+    if model.published? and !model.is_posted
       publish(model)
     end
   end
