5bd766ad409ce3dd575140e2260384d8eaf276a5
khodzha <khodzha.sh@gmail.com>
2012-07-27 10:35:32 +0300

    fix typos

--- app/controllers/fb_pages_controller.rb
+++ app/controllers/fb_pages_controller.rb
@@ -2,7 +2,7 @@
 class FbPagesController < ApplicationController
   
   def fb_auth
-    fb_auth = FbGraph::Auth.new "#{Rails.application.config.fb_app_id}", "#{Rails.application.config.fb_app_secret}", redirect_uri: '"#{Rails.application.config.host_name}/fb_pages'
+    fb_auth = FbGraph::Auth.new "#{Rails.application.config.fb_app_id}", "#{Rails.application.config.fb_app_secret}", redirect_uri: "#{Rails.application.config.host_name}/fb_pages"
     client = fb_auth.client
     redirect_to client.authorization_uri(
       :scope => [:manage_pages, :publish_stream, :offline_access]
@@ -10,7 +10,7 @@ class FbPagesController < ApplicationController
   end
 
   def fb_pages
-    fb_auth = FbGraph::Auth.new "#{Rails.application.config.fb_app_id}", "#{Rails.application.config.fb_app_secret}", redirect_uri: '"#{Rails.application.config.host_name}/fb_pages'
+    fb_auth = FbGraph::Auth.new "#{Rails.application.config.fb_app_id}", "#{Rails.application.config.fb_app_secret}", redirect_uri: "#{Rails.application.config.host_name}/fb_pages"
     client = fb_auth.client
     client.authorization_code = params[:code]
     access_token = client.access_token! :client_auth_body
