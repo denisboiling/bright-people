ab8cf344e2a3ecb19381caee9ea2a47ec5387766
Maxim Kremenev <ezotrank@gmail.com>
2012-08-20 19:04:02 +0300

    fix routes to change root to "photos#promo"

--- config/routes.rb
+++ config/routes.rb
@@ -98,7 +98,8 @@ BrightPeople::Application.routes.draw do
   match '/staff/add_video' => 'staff#add_video', :via => :post
   match '/staff/get_categories' => 'staff#get_categories'
 
-  root :to => 'home#show'
+  # root :to => 'home#show'
+  root :to => 'photos#promo'
 
   # TODO: dirty and don't why why migrate is broken?
   ActiveAdmin.routes(self) if ActiveRecord::Base.connection.table_exists?(:activity_comments)
