233bf916f290018681defd00c237971ee5831050
Maxim Kremenev <ezotrank@gmail.com>
2012-08-21 10:55:47 +0300

    try to remove change clock from photo.js to check HD

--- app/assets/javascripts/promo/photos.js
+++ app/assets/javascripts/promo/photos.js
@@ -64,30 +64,27 @@ VK.Widgets.Like("vk_like", {type: "button", height: 22}, {data_id});\
       $(this).stop().animate({ 'top': '-98px'}, 300)
     })
 
-    $('.bri-photographer')
-      .click(function()
-      {
+    $('.bri-photographer').click(function(){
         $(this).toggleClass('active')
         $(this).find('.bri-photo').slideToggle('fast')
         $(this).find('.bri-camera').slideToggle('fast')
 	$("#bri-form-photographers").val(window.choose_photographers());
-  $("#bri-form-photographers").change();
-  if ( $('.bri-photographer:not(.active)').length == 10 ) {
-      $('#bri-photographers-select-all').toggleClass('active')
-      $('#bri-photographers-select-all').html('Выбрать всех фотографов')
-  }
+	$("#bri-form-photographers").change();
+	
+	if ( $('.bri-photographer:not(.active)').length == 10 ) {
+	    $('#bri-photographers-select-all').toggleClass('active')
+	    $('#bri-photographers-select-all').html('Выбрать всех фотографов')
+	}
 
-  if ( $('.bri-photographer.active').length == 10 ) {
-      $('#bri-photographers-select-all').toggleClass('active')
-      $('#bri-photographers-select-all').html('Убрать всех фотографов')
-  }
-        return false
+	if ( $('.bri-photographer.active').length == 10 ) {
+	    $('#bri-photographers-select-all').toggleClass('active')
+	    $('#bri-photographers-select-all').html('Убрать всех фотографов')
+	}
+        
+	return false
       })
 
-    $('#bri-photographers-select-all')
-      .click(function()
-      {
-
+    $('#bri-photographers-select-all').click(function(){
     	$(this).toggleClass('active')
         if ($(this).hasClass('active')) { 
     	    $(this).html('Убрать всех фотографов')
@@ -134,50 +131,51 @@ VK.Widgets.Like("vk_like", {type: "button", height: 22}, {data_id});\
             $('#bri-form-minute').change()
           }
         )
-    var hour = parseInt($('#bri-form-hour').val());
-    var minute = parseInt($('#bri-form-minute').val());
-    var hour_angle = (hour - 10) * 30 - 420; // 420 and 360 are some magic numbers by author of the script
-    var minute_angle = (minute / 5) * 30 - 360;
+
+    // var hour = parseInt($('#bri-form-hour').val());
+    // var minute = parseInt($('#bri-form-minute').val());
+    // var hour_angle = (hour - 10) * 30 - 420; // 420 and 360 are some magic numbers by author of the script
+    // var minute_angle = (minute / 5) * 30 - 360;
 
 
-    hour_colours = ['#fff368','#ffec1d','#ffdd3f','#ffd22d','#fec905','#fcbf07','#ffb400','#ffa800','#f89c1e','#f1900c','#f8820f','#f9760c'];
-    base_hour_color = '#c94364';
+    // hour_colours = ['#fff368','#ffec1d','#ffdd3f','#ffd22d','#fec905','#fcbf07','#ffb400','#ffa800','#f89c1e','#f1900c','#f8820f','#f9760c'];
+    // base_hour_color = '#c94364';
 
 
-    minute_colours = ['#ebf736','#e1ed34','#d9e432','#cedb0e','#c7d40c','#c1ce08','#b7c708','#afd247','#a7ca3f','#90c119','#86bd00','#73bc00'];
-    base_minute_color = '#e4dacb';
-    // ЧТОБЫ ПОВЕРНУТЬ СТРЕЛКИ ЧАСОВ НА НУЖНОЕ ВРЕМЯ, МОЖНО ИСПОЛЬЗОВАТЬ КОСТЫЛЬ (ВСЯ СОЛЬ В r-60, ГДЕ -60 - ГРАДУС ПОВОРОТА)
-    clock.minuteHand.animate({
-        transform: 't'+clock.minuteHand.marginLeft+','+clock.minuteHand.marginTop+'r' + minute_angle + ','+clock.minuteHand.hx+','+clock.minuteHand.hy
-      },
-      2500,
-      '<>'
-    )
-    clock.hourHand.animate({
-        transform: 't'+clock.hourHand.marginLeft+','+clock.hourHand.marginTop+'r' + hour_angle + ','+clock.hourHand.hx+','+clock.hourHand.hy
-      },
-      1500,
-      '<>'
-    )
-    // КАК БЛЯТЬ РАСКРАСИТЬ ТО ЧТО ПОД НИМИ
-    // reset hours colour
-    var arr = $('svg path').slice(0, 12).toArray();
-    arr.reverse();
-    for(i = 0; i < arr.length; i++) {
-      $(arr[i]).attr('stroke', base_hour_color )
-    }
-    // set hour colours
-    $(arr[hour - 10]).attr('stroke', hour_colours[hour - 10]);
+    // minute_colours = ['#ebf736','#e1ed34','#d9e432','#cedb0e','#c7d40c','#c1ce08','#b7c708','#afd247','#a7ca3f','#90c119','#86bd00','#73bc00'];
+    // base_minute_color = '#e4dacb';
+    // // ЧТОБЫ ПОВЕРНУТЬ СТРЕЛКИ ЧАСОВ НА НУЖНОЕ ВРЕМЯ, МОЖНО ИСПОЛЬЗОВАТЬ КОСТЫЛЬ (ВСЯ СОЛЬ В r-60, ГДЕ -60 - ГРАДУС ПОВОРОТА)
+    // clock.minuteHand.animate({
+    //     transform: 't'+clock.minuteHand.marginLeft+','+clock.minuteHand.marginTop+'r' + minute_angle + ','+clock.minuteHand.hx+','+clock.minuteHand.hy
+    //   },
+    //   2500,
+    //   '<>'
+    // )
+    // clock.hourHand.animate({
+    //     transform: 't'+clock.hourHand.marginLeft+','+clock.hourHand.marginTop+'r' + hour_angle + ','+clock.hourHand.hx+','+clock.hourHand.hy
+    //   },
+    //   1500,
+    //   '<>'
+    // )
+    // // КАК БЛЯТЬ РАСКРАСИТЬ ТО ЧТО ПОД НИМИ
+    // // reset hours colour
+    // var arr = $('svg path').slice(0, 12).toArray();
+    // arr.reverse();
+    // for(i = 0; i < arr.length; i++) {
+    //   $(arr[i]).attr('stroke', base_hour_color )
+    // }
+    // // set hour colours
+    // $(arr[hour - 10]).attr('stroke', hour_colours[hour - 10]);
 
-    // set minutes colour
+    // // set minutes colour
 
-    arr = $('svg path').slice(12, 24).toArray();
-    arr.reverse();
-    for(i = 0; i < arr.length; i++) {
-      $(arr[i]).attr('stroke', base_minute_color )
-    }
-    // set minutes colours
-    $(arr[ minute / 5 ]).attr('stroke', minute_colours[minute/5]);
+    // arr = $('svg path').slice(12, 24).toArray();
+    // arr.reverse();
+    // for(i = 0; i < arr.length; i++) {
+    //   $(arr[i]).attr('stroke', base_minute_color )
+    // }
+    // // set minutes colours
+    // $(arr[ minute / 5 ]).attr('stroke', minute_colours[minute/5]);
 
-    // в задницу такие костыли
+    // // в задницу такие костыли
 }
