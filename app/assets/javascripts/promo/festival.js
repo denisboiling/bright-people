e9c34b819f6eb00bd87846e8e1185e2d297bb4f4
rodweb <rodweb@rodweb-z560.(none)>
2012-08-27 11:59:43 +0300

    Festival page fix

--- app/assets/javascripts/promo/festival.js
+++ app/assets/javascripts/promo/festival.js
@@ -1,64 +1,85 @@
 function festival ()
 {
-
   $(document).ready(function()
   {
-    function bezier(xa, ya, xb, yb, xc, yc, xd, yd)
-    {
-
-      return ['M'+xa, ya, 'C'+xb, yb, xc, yc, xd, yd].join(' ')
+    window.pretty_init_photo = function pretty_init(){
+  $("a[rel^='prettyPhoto']").prettyPhoto({
+      animation_speed: 'fast', /* fast/slow/normal */
+      slideshow: false, /* false OR interval time in ms */
+      autoplay_slideshow: false, /* true/false */
+      opacity: 0.80, /* Value between 0 and 1 */
+      show_title: false, /* true/false */
+      autoplay: false,
+      default_width: 500,
+      default_height: 344,
+      allow_resize: true,
+      theme: 'pp_default',
+      gallery_markup: '',
+      deeplinking: false,
+      callback: function(){
+    $('#bri-form-photo').val('');
+    window.push_history();
+      },
+      changepicturecallback: function(){
+    console.log("open");
+    window.fbAsyncInit();
+    window.vk_init("Яркие Люди", location.href, $("#pp_full_res img").attr("src"));
+    $("a.twitter-share-button").attr("data-url", location.href)
+    $.getScript('http://platform.twitter.com/widgets.js');
+      },
+      social_tools: '<div class="count_view"></div> \
+                           <div class="download_pic"><a></a></div> \
+                           <div class="pp_social"> \
+                             <div class="soc_like_button"> \
+                               <div class="fb-like" data-send="false" data-layout="button_count" data-width="450" data-show-faces="true"></div> \
+                             </div> \
+                             <div class="soc_like_button"> \
+                               <div id="vk_like"></div> \
+                             </div> \
+                             <div class="soc_like_button"> \
+                               <div id="vk_share"></div> \
+                             </div> \
+                             <div class="soc_like_button"> \
+                               <a href="https://twitter.com/share" class="twitter-share-button" data-lang="ru" data-url="" target="_blank">Твитнуть</a> \
+                             </div> \
+                             <div class="soc_like_button"> \
+                               <div class="g-plusone" data-size="medium" data-href="' + location.href + '"></div> \
+                             </div> \
+                           </div>'
+  })
     }
 
-    // Init SVG for drawing curves
-    var paper = Raphael( 'bri-bezier', $('#bri-fest-events').width(), $('#bri-fest-events').height() )
+    window.pretty_init_photo()
 
+    $('#am-container')
+      .montage(
+      {
+        minsize     : true
+      , fillLastRow : true
+      , fixedHeight : 110
+      , minw        : 110
+      })
 
     $('#bri-fest-events div.bri-item ')
       .mouseenter(function()
       {
         $('#bri-fest-events .bri-sub').hide()
 
-      //  $(this).parent('div').next().find('a').animate
-        //(
-        //  { fontSize: '150%', color: '#c74866' }, 150)
-
         $(this).find('a:first').stop().animate
         (
           { fontSize: '200%', color: '#c74866' }
         , 150
         , function ()
-          {
-            
+          {            
             var margin  = $(this).width()
             $(this).siblings('.bri-sub')
               .css({ left: margin+'px' })
               .show()
-/*
-            paper.setSize( $('#bri-fest-events').width(), $('#bri-fest-events').height() )
-
-            start = [ $(this).offset().left, $(this).offset().top+$(this).height()/2 ]
-
-            pos = []
-            $(this).siblings('.bri-sub').children('div')
-              .each(function()
-              {
-                pos.push( [ $(this).position().left+margin+100, start[1]+$(this).position().top-$(this).height()/2 ] )
-              })
-
-            for (var i=0; i<pos.length; i++)
-            {
-              console.log(start[0], start[1], pos[i][0]/2, start[1], pos[i][0]/2, pos[i][1], pos[i][0], pos[i][1])
-              paper.path(bezier( start[0], start[1], pos[i][0]/2, start[1], pos[i][0]/2, pos[i][1], pos[i][0], pos[i][1] ))
-                .attr('stroke','#c74866')
-            }
-            */
           }
         )
       })
       .mouseleave(function()
       {
-        paper.clear()
-
         $(this).find('.bri-sub').hide()
 
         $(this).find('a:first').stop().animate
@@ -66,30 +87,51 @@ function festival ()
           { fontSize: '100%' }
         , 150
         )
-
-
-        //$(this).parent('div').next().find('a').animate
-        //(
-       //   { fontSize: '100%' }
-      //  , 150
-      //  )
-
-      })
-
-
-
-
-      $(".cat_link").live("click", function(event){
-        event.preventDefault();
-        var category_title = $(this).text();
-        $.ajax({
-          url: "/festival",
-          data: { category: category_title },
-          success: function(data){
-            $('.container').html(data);
-          }
-        });
       });
+
+    $(".cat_link").live("click", function(event){
+      event.preventDefault();
+      var category_title = $(this).text();
+      $.ajax({
+        url: "/festival",
+        data: { category: category_title },
+        success: function(data){
+          $('.container').detach()
+          $('.clearfix')
+            .before('<div class="container am-container" id="am-container"></div>')
+            
+          $('#am-container').html(data)
+
+          var imgs   = $('#am-container').find('img')
+          ,   total  = imgs.length
+          ,   loaded = 0
+          
+          console.log(imgs)
+
+          imgs
+            .each(function()
+            {
+              $(this)
+                .load(function()
+                {
+                  loaded++
+                  if (loaded == total)
+                  {
+                    $('#am-container')
+                      .montage(
+                      {
+                        minsize     : true
+                      , fillLastRow : true
+                      , fixedHeight : 110
+                      , minw        : 110
+                      })
+                    window.pretty_init_photo()
+                  }
+                })
+            })
+        }
+      });
+    });
   })
 
 }
