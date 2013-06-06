fbaf8bacbd6419ba4f26db84c6e6ba72c65277db
novoselov <novsem@gmail.com>
2013-06-06 10:35:47 +0300

    archive: fix pages year migration - make it reversible

--- db/migrate/20130605122541_add_year_to_pages.rb
+++ db/migrate/20130605122541_add_year_to_pages.rb
@@ -1,8 +1,12 @@
 class AddYearToPages < ActiveRecord::Migration
-  def change
+  def up
     add_column :pages, :year, :integer
     
     Page.reset_column_information
     Page.all.each { |page| page.update_attributes!(year: 2012 ) }
+  end
+  
+  def down
+    remove_column :pages, :year
   end
 end
