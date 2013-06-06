b144a7c58378e232ba60a529e7bbf2f773d153ec
novoselov <novsem@gmail.com>
2013-06-06 17:42:49 +0300

    archive: show page strict by year

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,6 @@ class PagesController < HighVoltage::PagesController
     permlink = params[:id]
     year = params[:year] || Time.now.year
     @page = Page.where(year: year).as(permlink)
-    @page ||= Page.as(permlink)
     @year = year unless year == Time.now.year
   end
   
